3825242f8a2ebaf782e797f20fc04186e1f335e9
rwrtx <152061298+rwrtx@users.noreply.github.com>
2024-01-01 01:30:59 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,26 +1,3 @@
-#!/bin/bash
-#!/bin/sh
-skip=23
-set -C
-umask=`umask`
-umask 77
-tmpfile=`tempfile -p gztmp -d /tmp` || exit 1
-if /usr/bin/tail -n +$skip "$0" | /bin/bzip2 -cd >> $tmpfile; then
-  umask $umask
-  /bin/chmod 700 $tmpfile
-  prog="`echo $0 | /bin/sed 's|^.*/||'`"
-  if /bin/ln -T $tmpfile "/tmp/$prog" 2>/dev/null; then
-    trap '/bin/rm -f $tmpfile "/tmp/$prog"; exit $res' 0
-    (/bin/sleep 5; /bin/rm -f $tmpfile "/tmp/$prog") 2>/dev/null &
-    /tmp/"$prog" ${1+"$@"}; res=$?
-  else
-    trap '/bin/rm -f $tmpfile; exit $res' 0
-    (/bin/sleep 5; /bin/rm -f $tmpfile) 2>/dev/null &
-    $tmpfile ${1+"$@"}; res=$?
-  fi
-else
-  echo Cannot decompress $0; exit 1
-fi; exit $res
 BZh91AY&SY^^@^Mt                ^?b^T !^D@^P^P0^@.ܰ}5}^On˾ǫv >
 6Єz)Ȟ^M^C@^L^P^Z^Y=!^@^A^A"iL^TF^^^@^F@4^Z^@^@4^@^@^@^@IM^L ^MBO$>
 ^D%^L]}^U^Ct*"eAm[svDArhๆƮZ^aʩDo3X)*^Te7T!^H-JR:'>
